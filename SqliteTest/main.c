a59bc097efc27a3746d5e4b645271e96c8d4eb0f
yuliu <superliuyu1006@gmail.com>
2015-12-04 18:16:25 +0800

    Initial Commit

---
+++ SqliteTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SqliteTest
+//
+//  Created by liuyu on 15/12/4.
+//  Copyright © 2015年 liuyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
